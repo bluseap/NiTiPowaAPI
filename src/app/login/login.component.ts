0aa1f140212ba9e6d681da19afdf9600ad3d1c32
bluseap <khoinguyenaglx@mail.com>
2017-06-24 18:45:38 +0700

    login01

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -26,7 +26,7 @@ export class LoginComponent implements OnInit {
   }
 
   login() {    
-    this.loading = true;
+    
     this.notificationService.printSuccessMessage(MessageContstants.SYSTEM_THANHCONG);
     this.notificationService.printErrorMessage(MessageContstants.SYSTEM_ERROR_MSG);
     //this.router.navigate([UrlConstants.HOME]);
