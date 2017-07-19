ae6429aa03da63b82b3d6581377fa3e320c68225
bluseap <khoinguyenaglx@mail.com>
2017-07-19 23:15:12 +0700

    sidebar02

--- src/app/main/user/user.component.ts
+++ src/app/main/user/user.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgForm } from '@angular/forms';
+import { DataService } from '../../core/services/data.service';
+import { ModalDirective } from 'ngx-bootstrap/modal';
+import { NotificationService } from '../../core/services/notification.service';
+//import { UploadService } from '../../core/services/upload.service';
+//import { AuthenService } from '../../core/services/authen.service';
+import { UtilityService } from '../../core/services/utility.service';
+import { MessageContstants } from '../../core/common/message.constants';
+import { SystemConstants } from '../../core/common/system.constants';
+import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -7,9 +18,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  //@ViewChild('modalAddEdit') public modalAddEdit: ModalDirective;
+  //@ViewChild('avatar') avatar;
+  public myRoles: string[] = [];
+  public pageIndex: number = 1;
+  public pageSize: number = 20;
+  public pageDisplay: number = 10;
+  public totalRow: number;
+  public filter: string = '';
+  public users: any[];
+  public entity: any;
+  public baseFolder: string = SystemConstants.BASE_API;
+  public allRoles: IMultiSelectOption[] = [];
+  public roles: any[];
+
+  public dateOptions: any = {
+    locale: { format: 'DD/MM/YYYY' },
+    alwaysShowCalendars: false,
+    singleDatePicker: true
+  };
+
+  constructor(private _dataService: DataService,
+    private _notificationService: NotificationService,
+    private _utilityService: UtilityService,
+    //private _uploadService: UploadService,
+  //  public _authenService: AuthenService
+  ) {}
+
+    /*if (_authenService.checkAccess('USER') == false) {
+      _utilityService.navigateToLogin();
+    }*/
+ 
 
   ngOnInit() {
+
+    this.loadDataUser();
+  }
+
+  loadDataUser() {
+    //this._dataService.get('/api/useradmin/getlist?page=' + this.pageIndex + '&pageSize=' + this.pageSize + '&filter=' + this.filter)
+    this._dataService.get('/api/useradmin/getlist?page=1&pageSize=2&filter=')
+      .subscribe((response: any) => {
+        this.users = response;
+        this.pageIndex = response.PageIndex;
+        this.pageSize = response.PageSize;
+        this.totalRow = response.TotalRows;
+      });
   }
 
 }
--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -3,6 +3,17 @@ import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DataService } from '../../core/services/data.service';
+import { UtilityService } from '../../core/services/utility.service';
+import { NotificationService } from '../../core/services/notification.service';
+//import { UploadService } from '../../core/services/upload.service';
+
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { FormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { MultiselectDropdownModule } from 'angular-2-dropdown-multiselect';
+import { Daterangepicker } from 'ng2-daterangepicker';
+
 const userRoutes: Routes = [
    //localhost:4200/main/user
   { path: '', redirectTo: 'index', pathMatch: 'full' },
@@ -12,8 +23,14 @@ const userRoutes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    PaginationModule,
+    FormsModule,
+    MultiselectDropdownModule,
+    Daterangepicker,
+    ModalModule.forRoot(),
     RouterModule.forChild(userRoutes)
   ],
-  declarations: [UserComponent]
+  declarations: [UserComponent],
+  providers: [DataService, UtilityService, NotificationService]//,UploadService]
 })
 export class UserModule { }
